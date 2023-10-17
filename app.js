e26ca8da13584b662ff02b296a6f489fa379563d
Muhammad-Talha-Tahir <114994923+Talha971@users.noreply.github.com>
2023-10-17 23:13:27 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-
-function dabaya(e) {
-  document.getElementById("inp").value += e;
+function abc(e) {
+  var a = (document.getElementById("inp").value += e);
 }
-function eq() {
-  var a = document.getElementById("inp");
-  a.value = eval(a.value);
-}
-
-function clr() {
-  document.getElementById("inp").value = " ";
+function alclr() {
+  var a = (document.getElementById("inp").value = "");
 }
 function del() {
   var a = document.getElementById("inp");
   a.value = a.value.slice(0, -1);
+}
+function eq() {
+  var a = document.getElementById("inp");
+  a.value = eval(a.value);
 }
